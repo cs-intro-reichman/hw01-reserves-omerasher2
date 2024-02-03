74aff83d7377bb5ef8b346d3d67a9456731cdb66
omerasher2 <157235083+omerasher2@users.noreply.github.com>
2024-02-03 13:35:33 +0200

    fix

--- AddTwo.java
+++ AddTwo.java
@@ -4,8 +4,8 @@
 public class AddTwo {
 	public static void main(String[] args) {
 		int a, b;
-        a = 5;
-        b = 4;
+        a = Integer.parseInt(args[0]);
+        b = Integer.parseInt(args[1]);
         int c = a+b;
         System.out.println(a + " + " + b + " = " + c);
 	}
